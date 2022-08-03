9fc221e6c6afaed755d0d96268cf6bacd028506b
Mayur Mallav Saikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-03 14:08:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let http=require('http')                                        // Node Server
 http.createServer(function(req,res){
-    res.write('<h1>Hello Node Server !!</h1>')
+    res.write('<h1>Hello Node Server !!</h1><br><a href="progradexpress.herokuapp.com/express">Click to see Express Server results</a>')
     res.end()
      
-}).listen(8000,()=>{console.log('Node Server running at the port 8000')})
+}).listen(process.env.PORT || 8000,()=>{console.log('Node Server running at the port 8000')})
 
 
 
-let express=require('express')
+let express=require('express')                              // express server
 let app=express()
 
-app.get('/', function(req,res){
+app.get('/express', function(req,res){
 
     res.send('<h1>Hello Express Server !!</h1>')
 })
-app.listen(8050,()=>{console.log('Express Server running at port 8050')})
+app.listen(process.env.PORT || 8050,()=>{console.log('Express Server running at port 8050')})
