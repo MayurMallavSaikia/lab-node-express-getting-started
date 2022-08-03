7a15fc9e7b4bdb49d8a129c75e725f6f76ee3156
Mayur Mallav Saikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-03 15:41:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let http=require('http')                                        // Node Server
-http.createServer(function(req,res){
-    res.write('<h1>Hello Node Server !!</h1><br><a href="/express">Click to see Express Server results</a>')
-    res.end()
+// let http=require('http')                                        // Node Server
+// http.createServer(function(req,res){
+//     res.write('<h1>Hello Node Server !!</h1><br><a href="/express">Click to see Express Server results</a>')
+//     res.end()
      
-}).listen(process.env.PORT || 8000,()=>{console.log('Node Server running at the port 8000')})
+// }).listen(process.env.PORT || 8000,()=>{console.log('Node Server running at the port 8000')})
 
 
 
@@ -12,6 +12,6 @@ let app=express()
 
 app.get('/express', function(req,res){
 
-    res.send('<h1>Hello Express Server !!</h1>')
+    res.send('<h1>Hello Express Server !!</h1><br><a href="/express">Click to see Node Server results</a>')
 })
 app.listen(process.env.PORT || 8050,()=>{console.log('Express Server running at port 8050')})
